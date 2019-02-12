73b13afe7608e3c7bdd85bba9d56871a8ae739fa
Paul <38801847+plmaida@users.noreply.github.com>
2019-02-12 17:02:11 -0500

    update using black

--- expert_systems/Expert_System_Animal_Type.py
+++ expert_systems/Expert_System_Animal_Type.py
@@ -1,117 +1,135 @@
-#Question 2: Create an expert system if the animal type is
-#protozoa (single cell animal), invertebrae, fish, bird, mammal, unknown
+# Question 2: Create an expert system if the animal type is
+# protozoa (single cell animal), invertebrae, fish, bird, mammal, unknown
 
 
 import sys
 from pyknow import *
 
-#Create a function that determines if the answer is true or false
+# Create a function that determines if the answer is true or false
 def T_F(variable):
-    if(variable == "yes"):
+    if variable == "yes":
         answer = True
     else:
         answer = False
     return answer
 
-#Set up the classes
+
+# Set up the classes
 class Cell(Fact):
     """Is it a multi-cell organism or not (only protozoa are single celled)"""
+
     pass
+
 
 class Bones(Fact):
     """Does it have bones (only invertebraes do not have bones )"""
+
     pass
+
 
 class Gills(Fact):
     "Does it have gills (only fish have gills)"
     pass
 
+
 class Cover(Fact):
     "What is it's cover"
     pass
+
 
 class Wings(Fact):
     "Does it have wings"
     pass
 
+
 class AnimalType(KnowledgeEngine):
-    #Determine if the animal is a protozoa using a yes or no question. 
-    @Rule(Cell(cell= False))
+    # Determine if the animal is a protozoa using a yes or no question.
+    @Rule(Cell(cell=False))
     def protozoa(self):
         print("It's a protozoa")
-     
-    #determine if the animal is an invertebrae using a yes or no question. 
-    @Rule(
-        AND(
-             Cell(cell= True),
-             Bones(bones= False)))
+
+    # determine if the animal is an invertebrae using a yes or no question.
+    @Rule(AND(Cell(cell=True), Bones(bones=False)))
     def bones(self):
         print("It's an invertebrae")
-    
-    #determine if the animal is a fish using a yes or no question
-    @Rule(
-       AND(
-             Cell(cell= True),
-             Bones(bones= True),
-             Gills(gills= True)))
+
+    # determine if the animal is a fish using a yes or no question
+    @Rule(AND(Cell(cell=True), Bones(bones=True), Gills(gills=True)))
     def fish(self):
         print("It's a fish")
-    
-    #determine if the animal is a bird. Using the same questions from Q1. If it has wings and feathers then it is a bird.
+
+    # determine if the animal is a bird. Using the same questions from Q1. If it has wings and feathers then it is a bird.
     @Rule(
         AND(
-             Cell(cell= True),
-             Bones(bones= True),
-             Gills(gills= False), 
-             Cover(cover="feathers"),
-             Wings(wings= True)))
+            Cell(cell=True),
+            Bones(bones=True),
+            Gills(gills=False),
+            Cover(cover="feathers"),
+            Wings(wings=True),
+        )
+    )
     def bird(self):
         print("It's a bird")
-    
-    #determine if the animal is a mammal. Using the same questions from Q1. If it has fur it is a mammal.
+
+    # determine if the animal is a mammal. Using the same questions from Q1. If it has fur it is a mammal.
     @Rule(
         AND(
-             Cell(cell= True),
-             Bones(bones= True),
-             Gills(gills= False),
-             Cover(cover="fur"),
-             OR(Wings(wings= True),
-                Wings(wings= False))))
+            Cell(cell=True),
+            Bones(bones=True),
+            Gills(gills=False),
+            Cover(cover="fur"),
+            OR(Wings(wings=True), Wings(wings=False)),
+        )
+    )
     def mammal(self):
         print("It's a mammal")
-    
-    #determine if the animal is unknown using the same questions from Q1. If it has feathers, but not wings. 
+
+    # determine if the animal is unknown using the same questions from Q1. If it has feathers, but not wings.
     @Rule(
-       AND(
-             Cell(cell= True),
-             Bones(bones= True),
-             Gills(gills= False),
-             Cover(cover="feathers"),
-             Wings(wings= False)))
+        AND(
+            Cell(cell=True),
+            Bones(bones=True),
+            Gills(gills=False),
+            Cover(cover="feathers"),
+            Wings(wings=False),
+        )
+    )
     def uknown(self):
         print("Uknown")
-    
+
+
 system = AnimalType()
 system.reset()
 
-#define all initial questions as false. 
+# define all initial questions as false.
 cell_q = False
 bones_q = False
 gills_q = False
 cover_q = False
 wings_q = False
 
-#print("This program is case sensitive. Use 'Yes', 'No', 'fur', or 'feathers'")
+# print("This program is case sensitive. Use 'Yes', 'No', 'fur', or 'feathers'")
 
 cell_q = input("Is it multi-celled? (Yes or No) ").lower()
-if (cell_q == "yes"):   #if the animal is NOT multi-celled there is no reason to continue with the questions. 
+if (
+    cell_q == "yes"
+):  # if the animal is NOT multi-celled there is no reason to continue with the questions.
     bones_q = input("Does it have bones? (Yes or No) ").lower()
-    if (bones_q == "yes"): #if the animal does NOT have bones there is no reason to continue with the questions.
+    if (
+        bones_q == "yes"
+    ):  # if the animal does NOT have bones there is no reason to continue with the questions.
         gills_q = input("Does it Gills? (Yes or No) ").lower()
-        if (gills_q == "no"): #if the animal HAS gills there is no reason to continue with the questions.
+        if (
+            gills_q == "no"
+        ):  # if the animal HAS gills there is no reason to continue with the questions.
             cover_q = input("Does it have fur or feathers? ").lower()
-            wings_q = input("Does it have wings? (Yes or No) ").lower() 
+            wings_q = input("Does it have wings? (Yes or No) ").lower()
 
-system.declare(Cell(cell= T_F(cell_q)), Bones(bones= T_F(bones_q)), 
-               Gills(gills = T_F(gills_q)), Cover(cover= cover_q), Wings(wings= T_F(wings_q)))
+system.declare(
+    Cell(cell=T_F(cell_q)),
+    Bones(bones=T_F(bones_q)),
+    Gills(gills=T_F(gills_q)),
+    Cover(cover=cover_q),
+    Wings(wings=T_F(wings_q)),
+)
 system.run()
--- expert_systems/Hockey_Draft_Expert_System.py
+++ expert_systems/Hockey_Draft_Expert_System.py
@@ -1,158 +1,195 @@
 import sys
 from pyknow import *
 
-#Create a function that determines if the answer is true or false
+# Create a function that determines if the answer is true or false
 def T_F(variable):
-    if(variable == "yes"):
+    if variable == "yes":
         answer = True
     else:
         answer = False
     return answer
 
+
 class Hockey_IQ(Fact):
     pass
+
 
 class Skating(Fact):
     pass
 
+
 class LineMate(Fact):
     pass
+
 
 class Off_driver(Fact):
     pass
 
+
 class Def_aware(Fact):
     pass
+
 
 class Position(Fact):
     pass
 
+
 class DraftRound(KnowledgeEngine):
-    #drafting forwards
+    # drafting forwards
     @Rule(
         AND(
-            OR(Position(positions = "forward"), Position(positions = "defense")),
-            Hockey_IQ(hockey_iq = "high"),
-            Skating(skating = "high"),
-            LineMate(linemate = True),
-            Off_driver(off_driver = "high"),
-            Def_aware(def_aware = "high")))
+            OR(Position(positions="forward"), Position(positions="defense")),
+            Hockey_IQ(hockey_iq="high"),
+            Skating(skating="high"),
+            LineMate(linemate=True),
+            Off_driver(off_driver="high"),
+            Def_aware(def_aware="high"),
+        )
+    )
     def first_f(self):
         print("Draft this player in the first round")
-    
+
     @Rule(
         OR(
             AND(
-                Position(positions = "forward"),
-                Hockey_IQ(hockey_iq = "high"),
-                Skating(skating = "high"),
-                LineMate(linemate = True),
-                Off_driver(off_driver = "high"),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium"))),
+                Position(positions="forward"),
+                Hockey_IQ(hockey_iq="high"),
+                Skating(skating="high"),
+                LineMate(linemate=True),
+                Off_driver(off_driver="high"),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
             AND(
-                Position(positions = "defense"),
-                Hockey_IQ(hockey_iq = "high"),
-                Skating(skating = "high"),
-                LineMate(linemate = True),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium")),
-                Def_aware(def_aware = "high"))))
+                Position(positions="defense"),
+                Hockey_IQ(hockey_iq="high"),
+                Skating(skating="high"),
+                LineMate(linemate=True),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+                Def_aware(def_aware="high"),
+            ),
+        )
+    )
     def second_f(self):
         print("Draft this player in the second round")
-    
-  
+
     @Rule(
         OR(
             AND(
-                Position(positions = "forward"),
-                Hockey_IQ(hockey_iq = "high"),
-                OR(Skating(skating = "high"), Skating(skating="medium")),
-                LineMate(linemate = True),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium")),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium"))),
+                Position(positions="forward"),
+                Hockey_IQ(hockey_iq="high"),
+                OR(Skating(skating="high"), Skating(skating="medium")),
+                LineMate(linemate=True),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
             AND(
-                Position(positions = "defense"),
-                Hockey_IQ(hockey_iq = "high"),
-                Skating(skating = "high"),
-                LineMate(linemate = True),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium")),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium")))))
+                Position(positions="defense"),
+                Hockey_IQ(hockey_iq="high"),
+                Skating(skating="high"),
+                LineMate(linemate=True),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
+        )
+    )
     def third_f(self):
         print("Draft this player in the third round")
 
     @Rule(
         OR(
             AND(
-                Position(positions = "forward"),
-                Hockey_IQ(hockey_iq = "high"),
-                OR(Skating(skating = "high"),Skating(skating= "medium")),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium")),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium"))),
+                Position(positions="forward"),
+                Hockey_IQ(hockey_iq="high"),
+                OR(Skating(skating="high"), Skating(skating="medium")),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
             AND(
-                Position(positions = "defense"),
-                Hockey_IQ(hockey_iq = "high"),
-                Skating(skating = "high"),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium")),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium")))))
+                Position(positions="defense"),
+                Hockey_IQ(hockey_iq="high"),
+                Skating(skating="high"),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
+        )
+    )
     def fourth_f(self):
         print("Draft this player in the fourth round")
 
-    
     @Rule(
         OR(
             AND(
-                Position(positions = "forward"),
-                Hockey_IQ(hockey_iq = "high"),
-                OR(Skating(skating = "high"), Skating(skating="medium")),
-                OR(Off_driver(off_driver = "high"), Off_driver(off_driver = "medium"))),
+                Position(positions="forward"),
+                Hockey_IQ(hockey_iq="high"),
+                OR(Skating(skating="high"), Skating(skating="medium")),
+                OR(Off_driver(off_driver="high"), Off_driver(off_driver="medium")),
+            ),
             AND(
-                Position(positions = "defense"),
-                Hockey_IQ(hockey_iq = "high"),
-                OR(Skating(skating = "high"), Skating(skating="medium")),
-                OR(Def_aware(def_aware = "high"), Def_aware(def_aware = "medium")))))
+                Position(positions="defense"),
+                Hockey_IQ(hockey_iq="high"),
+                OR(Skating(skating="high"), Skating(skating="medium")),
+                OR(Def_aware(def_aware="high"), Def_aware(def_aware="medium")),
+            ),
+        )
+    )
     def fifth_f(self):
         print("Draft this player in the fifth round")
 
     @Rule(
         AND(
-            OR(Position(positions = "forward"),Position(position = "defense")),
-            OR(Hockey_IQ(hockey_iq = "high"), Hockey_IQ(hockey_iq = "medium")),
-            OR(Skating(skating = "high"), Skating(skating="medium"))))
-
+            OR(Position(positions="forward"), Position(position="defense")),
+            OR(Hockey_IQ(hockey_iq="high"), Hockey_IQ(hockey_iq="medium")),
+            OR(Skating(skating="high"), Skating(skating="medium")),
+        )
+    )
     def sixth_f(self):
         print("Draft this player in the sixth round")
-        
+
     @Rule(
         AND(
-            OR(Position(positions = "forward"),Position(position = "defense")),
-            OR(Hockey_IQ(hockey_iq = "high"), Hockey_IQ(hockey_iq = "medium"))))
+            OR(Position(positions="forward"), Position(position="defense")),
+            OR(Hockey_IQ(hockey_iq="high"), Hockey_IQ(hockey_iq="medium")),
+        )
+    )
     def seventh_f(self):
         print("Draft this player in the seventh round")
-        
-    @Rule(Hockey_IQ(hockey_iq = "low"))
+
+    @Rule(Hockey_IQ(hockey_iq="low"))
     def DND_f(self):
         print("Do not draft this player")
-        
-         
+
+
 system = DraftRound()
 system.reset()
 
 linemate_q = False
 
 print("All rankings should be high medium or low")
-position_q = input("What position does this player play? (Forward, Defense or Goalie) ").lower()
-if(position_q == "goalie"):
-    print("This expert system does not rank goalies due to their unpredictability! Pick a different player")
-#elif(position_q == "forward" or position_q == "defense"):
+position_q = input(
+    "What position does this player play? (Forward, Defense or Goalie) "
+).lower()
+if position_q == "goalie":
+    print(
+        "This expert system does not rank goalies due to their unpredictability! Pick a different player"
+    )
+# elif(position_q == "forward" or position_q == "defense"):
 else:
     hockey_iq_q = input("Rate the players hockey IQ: ").lower()
     skating_q = input("Ratethe players skating ability: ").lower()
-    linemate_q = input("Is the player the best player on their line? (Yes or No)").lower()
+    linemate_q = input(
+        "Is the player the best player on their line? (Yes or No)"
+    ).lower()
     off_driver_q = input("Rate the player's ability to drive offense: ").lower()
     def_aware_q = input("Rate the player's defensive awareness: ").lower()
-    
-    system.declare(Position(positions = position_q), Hockey_IQ(hockey_iq= hockey_iq_q), Skating(skating= skating_q), 
-             LineMate(linemate = T_F(linemate_q)), Off_driver(off_driver= off_driver_q), Def_aware(def_aware= def_aware_q))
+
+    system.declare(
+        Position(positions=position_q),
+        Hockey_IQ(hockey_iq=hockey_iq_q),
+        Skating(skating=skating_q),
+        LineMate(linemate=T_F(linemate_q)),
+        Off_driver(off_driver=off_driver_q),
+        Def_aware(def_aware=def_aware_q),
+    )
     system.run()
-#else:
+# else:
 #    print("You have made an error. Please try again!")
-   
